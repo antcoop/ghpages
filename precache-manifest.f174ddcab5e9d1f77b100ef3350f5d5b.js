74b88e4497031d7ed730271164d446f926f03ad3
Anthony Cooper <iam@anthonycooper.me>
2020-07-21 13:13:00 -0500

    Updates

---
+++ precache-manifest.f174ddcab5e9d1f77b100ef3350f5d5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6352178a3ad033390bf795b9b1921c",
+    "url": "/ghpages/index.html"
+  },
+  {
+    "revision": "e9b4e8347be584c1da63",
+    "url": "/ghpages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4e42bd9041f1d1347295",
+    "url": "/ghpages/static/js/2.9a6dca9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghpages/static/js/2.9a6dca9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9b4e8347be584c1da63",
+    "url": "/ghpages/static/js/main.5f7b336f.chunk.js"
+  },
+  {
+    "revision": "71807ad094a937522751",
+    "url": "/ghpages/static/js/runtime-main.b93fca0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
